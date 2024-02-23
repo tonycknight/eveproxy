dc1542a9b430090b595cc0e7f996d0492386f10f
tonycknight <tonycknight@users.noreply.github.com>
2024-02-23 00:11:12 +0000

    move all stats to a single route

--- src/eveproxy/WebApp.fs
+++ src/eveproxy/WebApp.fs
@@ -1,7 +1,9 @@
 ﻿namespace eveproxy
 
 open System
+open eveproxy.zkb
 open Giraffe
+open Microsoft.AspNetCore.Http
 
 module WebApp =
 
@@ -11,11 +13,41 @@ module WebApp =
         >=> ResponseCaching.publicResponseCaching 999999 None
         >=> Successful.NO_CONTENT
 
-    let heartbeat =
+    let private heartbeat =
         GET
-        >=> route "/heartbeat"
+        >=> route "/heartbeat/"
         >=> ResponseCaching.noResponseCaching
         >=> json [ "OK" ]
+
+    let private stats =
+
+        fun (next: HttpFunc) (ctx: HttpContext) ->
+            task {
+                let statsActor = ctx.GetService<IApiStatsActor>()
+                let sessionsActor = ctx.GetService<ISessionsActor>()
+
+                let! statsActorStats = statsActor.GetStats()
+                let! apiStats = statsActor.GetApiStats()
+
+                let! ingestActorStats = ctx.GetService<IRedisqIngestionActor>().GetStats()
+                let! sessionsActorStats = sessionsActor.GetStats()
+                let! passthruStats = ctx.GetService<IZkbApiPassthroughActor>().GetStats()
+
+                let! kmCount = ctx.GetService<IKillmailRepository>().GetCountAsync()
+                let! sessionStorageStats = sessionsActor.GetStorageStats()
+
+                let result =
+                    {| actors = [| statsActorStats; ingestActorStats; sessionsActorStats; passthruStats |]
+                       killmails =
+                        {| ingestion = apiStats.ingestion
+                           distribution = apiStats.distribution
+                           storage =
+                            {| kills = kmCount
+                               sessions = sessionStorageStats |} |}
+                       routes = apiStats.routes |> Map.values |> Seq.sortByDescending (fun rs -> rs.count) |}
+
+                return! Successful.OK result next ctx
+            }
 
     let webApp (sp: IServiceProvider) =
         Api.isAuthorised sp
@@ -27,5 +59,6 @@ module WebApp =
                           (choose
                               [ choose
                                     [ heartbeat
+                                      route "/stats/" >=> stats
                                       eveproxy.zkb.Api.redisqWebRoutes ()
                                       eveproxy.zkb.Api.zkbWebRoutes () ] ]) ]
