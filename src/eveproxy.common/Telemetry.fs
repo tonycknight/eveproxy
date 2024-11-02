3bb892659ae4c19fb6465cb8f805ede16f352bb9
tonycknight <tonycknight@users.noreply.github.com>
2024-11-02 22:36:53 +0000

    MetricsTelemetry

---
+++ src/eveproxy.common/Telemetry.fs
@@ -0,0 +1,22 @@
+﻿namespace eveproxy
+
+open System
+open System.Diagnostics.Metrics
+
+type IMetricsTelemetry =
+    inherit IDisposable
+    abstract member ReceivedKillmails: int -> unit
+
+type MetricsTelemetry(meterFactory: IMeterFactory) =
+            
+    [<Literal>]
+    let killmailType = "Killmail"
+
+    let ingestedKillmailMeter = meterFactory.Create("eveproxy_killmails")
+    let killmailReceivedMeter = ingestedKillmailMeter.CreateCounter<int>("eveproxy_received_killmails", killmailType)
+
+    interface IMetricsTelemetry with
+        member this.Dispose () = 
+            ingestedKillmailMeter.Dispose()
+            
+        member this.ReceivedKillmails count = killmailReceivedMeter.Add count
