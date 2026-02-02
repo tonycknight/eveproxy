7fc4513a8cb6ff536d3ca36ea76e3a28d0624981
Tony Knight <tonycknight@users.noreply.github.com>
2026-02-02 01:11:53 +0000

    fix disposable

--- src/eveproxy.common/Mongo.fs
+++ src/eveproxy.common/Mongo.fs
@@ -50,7 +50,8 @@ module Mongo =
         | x -> $"""{connectionString |> Strings.appendIfMissing "/"}{dbName}"""
 
     let initDb dbName (connection: string) =
-        let client = MongoClient(connection)
+        use client = new MongoClient(connection)
+        
         let db = client.GetDatabase(dbName)
 
         try
