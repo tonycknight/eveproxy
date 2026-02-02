027d472b28786960ca68ac2fd0d6a62b696f1628
Tony Knight <tonycknight@users.noreply.github.com>
2026-02-02 01:21:03 +0000

    upgrade otel dependencies

--- src/eveproxy.common/Mongo.fs
+++ src/eveproxy.common/Mongo.fs
@@ -50,7 +50,7 @@ module Mongo =
         | x -> $"""{connectionString |> Strings.appendIfMissing "/"}{dbName}"""
 
     let initDb dbName (connection: string) =
-        let client = new MongoClient(connection)
+        let client = new MongoClient(connection) // TODO: disposable
         
         let db = client.GetDatabase(dbName)
 
